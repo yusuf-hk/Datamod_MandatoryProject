50450d1d7f1e824e19619002367138f28ae23894
Yusuf Harmankaya <yusufhk96@gmail.com>
2019-11-18 12:48:03 +0100

    Polished task 2

--- oppgave 2.sql
+++ oppgave 2.sql
@@ -2,9 +2,9 @@
  Task 2
  */
 
-SELECT Address as Library_Address, sum(copies) as quantity
+SELECT Name as Library_Name, sum(copies) as quantity
 FROM LibraryBook
 INNER JOIN LibraryBranch LB on LibraryBook.LibraryBranchID = LB.LibraryBranchID
 INNER JOIN Book B on LibraryBook.bookID = B.BookID
 WHERE B.Title='User-friendly multi-state function'
-GROUP BY Address
+GROUP BY Library_Name
--- oppgave 4.sql
+++ oppgave 4.sql
@@ -6,4 +6,4 @@ FROM Borrower
 INNER JOIN Loan L on Borrower.BorrowerID = L.BorrowerID
 INNER JOIN Book B on L.bookID = B.BookID
 INNER JOIN LibraryBranch LB on L.LibraryBranchID = LB.LibraryBranchID
-WHERE L."Due Date"=11-18-2019 AND LB.Name = 'Ålesund'
+WHERE L."Due Date"=11-18-2019 AND LB.Name = 'Trondheim'
