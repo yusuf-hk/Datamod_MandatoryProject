fad652292ea49bb8e2fb88464df86ae76764f501
Yusuf Harmankaya <yusufhk96@gmail.com>
2019-11-18 10:11:05 +0100

    Done task 1

--- Oppgave 1.sql
+++ Oppgave 1.sql
@@ -1,5 +1,8 @@
-SELECT Name, copies
+/*
+ Task 4
+ */
+SELECT Name, sum(copies)
 FROM LibraryBook
 INNER JOIN Book B on LibraryBook.bookID = B.BookID
 INNER JOIN LibraryBranch LB on LibraryBook.LibraryBranchID = LB.LibraryBranchID
-WHERE B.Title='Profound non-volatile projection' AND B.Publisher='Krajcik, Rolfson and Quitzon';
+WHERE B.Title='User-friendly multi-state function' AND B.Publisher='Mills, Morissette and Waelchi' AND LB.Address = 'Larsgårdsvegen 2'
